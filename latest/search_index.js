37913702c5cd117d7a43576fec140b1abe0dee64
autodocs <autodocs>
2018-03-07 16:46:59 +0000

    build based on cc922ca

--- latest/search_index.js
+++ latest/search_index.js
@@ -28,72 +28,72 @@ var documenterSearchIndex = {"docs": [
     "location": "index.html#Observables.Observable",
     "page": "Home",
     "title": "Observables.Observable",
-    "category": "Type",
-    "text": "Like a Ref but updates can be watched by adding a handler using on.\n\n\n\n"
+    "category": "type",
+    "text": "Like a Ref but updates can be watched by adding a handler using on.\n\n\n\n\n\n"
 },
 
 {
-    "location": "index.html#Observables.on-Tuple{Any,Observables.Observable}",
+    "location": "index.html#Observables.on-Tuple{Any,Observable}",
     "page": "Home",
     "title": "Observables.on",
-    "category": "Method",
-    "text": "on(f, o::Observable)\n\nAdds function f as listener to o. Whenever o's value is set via o[] = val f is called with val.\n\n\n\n"
+    "category": "method",
+    "text": "on(f, o::Observable)\n\nAdds function f as listener to o. Whenever o\'s value is set via o[] = val f is called with val.\n\n\n\n\n\n"
 },
 
 {
-    "location": "index.html#Observables.off-Tuple{Observables.Observable,Any}",
+    "location": "index.html#Observables.off-Tuple{Observable,Any}",
     "page": "Home",
     "title": "Observables.off",
-    "category": "Method",
-    "text": "off(o::Observable, f)\n\nRemoves f from listeners of o.\n\n\n\n"
+    "category": "method",
+    "text": "off(o::Observable, f)\n\nRemoves f from listeners of o.\n\n\n\n\n\n"
 },
 
 {
-    "location": "index.html#Base.setindex!-Tuple{Observables.Observable,Any}",
+    "location": "index.html#Base.setindex!-Tuple{Observable,Any}",
     "page": "Home",
     "title": "Base.setindex!",
-    "category": "Method",
-    "text": "o[] = val\n\nUpdates the value of an Observable to val and call its listeners.\n\n\n\n"
+    "category": "method",
+    "text": "o[] = val\n\nUpdates the value of an Observable to val and call its listeners.\n\n\n\n\n\n"
 },
 
 {
-    "location": "index.html#Base.getindex-Tuple{Observables.Observable}",
+    "location": "index.html#Base.getindex-Tuple{Observable}",
     "page": "Home",
     "title": "Base.getindex",
-    "category": "Method",
-    "text": "o[]\n\nReturns the current value of o.\n\n\n\n"
+    "category": "method",
+    "text": "o[]\n\nReturns the current value of o.\n\n\n\n\n\n"
 },
 
 {
     "location": "index.html#Observables.onany-Tuple{Any,Vararg{Any,N} where N}",
     "page": "Home",
     "title": "Observables.onany",
-    "category": "Method",
-    "text": "onany(f, args...)\n\nCalls f on updates to any oservable refs in args. args may contain any number of Observable ojects. f will be passed the values contained in the refs as the respective argument. All other ojects in args are passed as-is.\n\n\n\n"
+    "category": "method",
+    "text": "onany(f, args...)\n\nCalls f on updates to any oservable refs in args. args may contain any number of Observable ojects. f will be passed the values contained in the refs as the respective argument. All other ojects in args are passed as-is.\n\n\n\n\n\n"
 },
 
 {
-    "location": "index.html#Base.map!-Tuple{Any,Observables.Observable,Vararg{Any,N} where N}",
+    "location": "index.html#Base.map!-Tuple{Any,Observable,Vararg{Any,N} where N}",
     "page": "Home",
     "title": "Base.map!",
-    "category": "Method",
-    "text": "map!(f, o::Observable, args...)\n\nUpdates o with the result of calling f with values extracted from args. args may contain any number of Observable ojects. f will be passed the values contained in the refs as the respective argument. All other ojects in args are passed as-is.\n\n\n\n"
+    "category": "method",
+    "text": "map!(f, o::Observable, args...)\n\nUpdates o with the result of calling f with values extracted from args. args may contain any number of Observable ojects. f will be passed the values contained in the refs as the respective argument. All other ojects in args are passed as-is.\n\n\n\n\n\n"
 },
 
 {
-    "location": "index.html#Observables.connect!-Tuple{Observables.Observable,Observables.Observable}",
+    "location": "index.html#Observables.connect!-Tuple{Observable,Observable}",
     "page": "Home",
     "title": "Observables.connect!",
-    "category": "Method",
-    "text": "connect!(o1::Observable, o2::Observable)\n\nForward all updates to o1 to o2\n\n\n\n"
+    "category": "method",
+    "text": "connect!(o1::Observable, o2::Observable)\n\nForward all updates to o1 to o2\n\n\n\n\n\n"
 },
 
 {
-    "location": "index.html#Base.map-Tuple{Any,Observables.Observable,Vararg{Any,N} where N}",
+    "location": "index.html#Base.map-Tuple{Any,Observable,Vararg{Any,N} where N}",
     "page": "Home",
     "title": "Base.map",
-    "category": "Method",
-    "text": "map(f, o::Observable, args...)\n\nCreates a new oservable ref which contains the result of f applied to values extracted from args. The second argument o must be an oservable ref for dispatch reasons. args may contain any number of Observable ojects. f will be passed the values contained in the refs as the respective argument. All other ojects in args are passed as-is.\n\n\n\n"
+    "category": "method",
+    "text": "map(f, o::Observable, args...)\n\nCreates a new oservable ref which contains the result of f applied to values extracted from args. The second argument o must be an oservable ref for dispatch reasons. args may contain any number of Observable ojects. f will be passed the values contained in the refs as the respective argument. All other ojects in args are passed as-is.\n\n\n\n\n\n"
 },
 
 {
