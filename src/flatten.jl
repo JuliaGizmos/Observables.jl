a8fbbf7fbe3093a9814e3109f1544b7e19d30ab6
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2018-08-18 12:34:56 +0100

    nested flatten

--- src/flatten.jl
+++ src/flatten.jl
@@ -1,5 +1,6 @@
 mutable struct Flatten <: AbstractObservable{Any}
     o::AbstractObservable
+    list::Vector{Tuple{AbstractObservable, Function}}
     inner_id::String
     output::Observable{Any}
     pair::ObservablePair
@@ -8,9 +9,37 @@ mutable struct Flatten <: AbstractObservable{Any}
         inner_id = obsid(inner_obs)
         output = Observable{Any}(inner_obs[])
         p = ObservablePair(inner_obs, output)
-        n = new(o, inner_id, output, p)
-        recursive_connect!(o, n)
+        n = new(o, Tuple{AbstractObservable, Function}[], inner_id, output, p)
+        for i in n
+            f = updater(i, n)
+            on(f, i)
+            push!(n.list, (i, f))
+        end
         n
+    end
+end
+
+function updater(i, f::Flatten)
+    function (val)
+        ind = findfirst(t -> obsid(t[1]) == obsid(i), f.list)
+        if ind !== nothing
+            for ii in length(f.list):-1:(ind+1)
+                obs, func = pop!(f.list)
+                off(obs, func)
+            end
+            while val isa AbstractObservable
+                func = updater(val, f)
+                on(func, val)
+                push!(f.list, (val, func))
+                i, val = val, i[]
+            end
+            if obsid(i) != f.inner_id
+                off(f.pair)
+                f[] = i[]
+                f.pair = ObservablePair(i, f.output)
+                f.inner_id = obsid(i)
+            end
+        end
     end
 end
 
@@ -25,22 +54,6 @@ function inner_observable(o::AbstractObservable)
         o = x
     end
     o
-end
-
-recursive_connect!(i, f::Flatten) = nothing
-function recursive_connect!(i::AbstractObservable, f::Flatten)
-    on(i) do val
-        if val isa AbstractObservable && i in f
-            inner_obs = inner_observable(val)
-            if obsid(inner_obs) != f.inner_id
-                off(f.pair)
-                f[] = inner_obs[]
-                f.pair = ObservablePair(inner_obs, f.output)
-                f.inner_id = obsid(inner_obs)
-            end
-            recursive_connect!(val, f)
-        end
-    end
 end
 
 flatten(x) = x
